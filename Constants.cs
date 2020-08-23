5317900ca712f875ec404228dd1d98d36d90d0f5
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2020-08-23 17:21:49 +0300

    Uploading the core backend algorithms

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Net_Diagnose
+{
+    class Constants
+    {
+        public const int NUM_OF_TARGETS = 4;
+        public const int MAX_TTL = 50;
+        public const int TIMEOUT = 5000;
+        public const int MAX_LATENCY = 100;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Net_Diagnose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network handler = new Network();
+            Console.WriteLine(handler.Gateway_Check());
+            Console.ReadKey();
+        }
+    }
+}

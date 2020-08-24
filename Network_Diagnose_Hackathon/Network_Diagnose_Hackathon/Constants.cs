024d24220505669260adda1a395173eba16ec6b4
Tony Malinkovich <39405726+11tonytony11@users.noreply.github.com>
2020-08-24 09:13:49 +0300

    Added another constant

--- Network_Diagnose_Hackathon/Network_Diagnose_Hackathon/Constants.cs
+++ Network_Diagnose_Hackathon/Network_Diagnose_Hackathon/Constants.cs
@@ -1,7 +1,8 @@
-﻿namespace Network_Diagnose_Hackathon
+namespace Network_Diagnose_Hackathon
 {
     class Constants
     {
+        public const int LENGTH = 2;
         public const int NUM_OF_TARGETS = 4;
         public const int MAX_TTL = 50;
         public const int TIMEOUT = 5000;

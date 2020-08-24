76ef60e9b48e07ae0e29c7af23cd7edb4e239a2b
Daniel Zigler <daniel.zsh@gmail.com>
2020-08-24 09:59:16 +0300

    added Database class

--- Network_Diagnose_Hackathon/Network_Diagnose_Hackathon/Database.cs
+++ Network_Diagnose_Hackathon/Network_Diagnose_Hackathon/Database.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 
 namespace Network_Diagnose_Hackathon
 {
     class Database
     {
+        SQLiteConnection m_dbConnection;
+        string file;
+
+        public Database (string path)
+        {
+            this.file = path;
+
+            if (!File.Exists(this.file)) this.createDB(this.file);
+            this.OpenDB();
+        }
+
+        private void createDB(string path) // file type is .sqlite
+        {
+            SQLiteConnection.CreateFile(path);
+            this.file = path;
+        }
+
+        private void OpenDB()
+        {
+            this.m_dbConnection = new SQLiteConnection("Data Source=" + this.file + ";Version=3;");
+            m_dbConnection.Open();
+        }
+
+        private SQLiteDataReader ExecuteQuery(string query)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(query, m_dbConnection);
+            cmd.ExecuteNonQuery();
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
 
     }
 }
